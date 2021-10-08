c1f53f38832fe306a366b5f4672836b26b7b3aa6
Jsowmya08 <sowmya.jangam0807@gmail.com>
2021-10-08 16:03:37 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace employee_present_or_abscent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int value=random.Next(0, 2);
+
+            if (value == isPresent)
+            {
+                Console.WriteLine("is present");
+            }
+            else
+            {
+                Console.WriteLine("is absent");
+            }
+        }
+    }
+}
